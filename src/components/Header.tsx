48ba945b8a5af3bf4113ce11ac6e24a4dd535a95
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-06-13 18:25:53 +0400

    add user icon in Header component for small screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { BsStars } from 'react-icons/bs'
+import { FaUserAlt } from 'react-icons/fa'
 
 const Header = () => {
 	return (
 		<header className='header'>
+			<button className='header__user'>
+				<FaUserAlt />
+			</button>
 			<h1 className='header__title'>Home</h1>
 			<button className='header__icon'>
 				<BsStars />
