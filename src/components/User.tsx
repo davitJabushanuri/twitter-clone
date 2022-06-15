8d3f870e627ad96b695c34a226684e59aa040389
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-06-15 23:19:29 +0400

    style user component for desktops

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -5,15 +5,20 @@ import { FaUserAlt } from 'react-icons/fa'
 const User = () => {
 	return (
 		<div className='user'>
-			<button className='user__avatar'>
-				<FaUserAlt />
-			</button>
-			<div className='user__info'>
-				<span className='user__info__name'>John Doe</span>
-				<span className='user__info__username'>@John1995</span>
+			<div className='user__photo'>
+				<button className='user__photo__avatar'>
+					<FaUserAlt />
+				</button>
 			</div>
-			<div className='user__button'>
-				<FiMoreHorizontal />
+
+			<div className='user__bio'>
+				<div className='user__bio__info'>
+					<span className='user__bio__info__name'>John Doe</span>
+					<span className='user__bio__info__username'>@John1995</span>
+				</div>
+				<button className='user__bio__button'>
+					<FiMoreHorizontal />
+				</button>
 			</div>
 		</div>
 	)
