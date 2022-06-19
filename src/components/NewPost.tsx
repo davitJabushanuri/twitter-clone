a4ab59322087c89bac6d403223411c2412f83b76
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-06-20 03:55:28 +0400

    add image support to newPost component

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -3,28 +3,59 @@ import { AiOutlinePicture, AiOutlineGif } from 'react-icons/ai'
 import { BsEmojiSmile } from 'react-icons/bs'
 import { HiOutlineLocationMarker } from 'react-icons/hi'
 
-import { useState } from 'react'
-import { db } from '../../firebase'
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { useState, useRef } from 'react'
+import { db, storage } from '../../firebase'
+import {
+	addDoc,
+	collection,
+	serverTimestamp,
+	updateDoc,
+	doc,
+} from 'firebase/firestore'
 
 import { useSession } from 'next-auth/react'
+import { getDownloadURL, ref, uploadString } from 'firebase/storage'
 
 const NewPost = () => {
 	const { data: session } = useSession()
-	console.log(session)
 
 	const [post, setPost] = useState('')
+	const [image, setImage] = useState(null)
+	const postPhotoRef = useRef(null)
+
+	const addImage = (e: any) => {
+		const reader = new FileReader()
+		if (e.target.files[0]) {
+			reader.readAsDataURL(e.target.files[0])
+		}
+		reader.onload = readerEvent => {
+			setImage(readerEvent.target.result)
+		}
+	}
 
 	const savePost = async () => {
-		console.log(post)
 		const docRef = addDoc(collection(db, 'posts'), {
 			text: post,
 			user: session!.user!.name,
 			username: session!.user!.username,
 			userImage: session!.user!.image,
-			userId: session!.user!.id,
+			id: session!.user!.id,
 			createdAt: serverTimestamp(),
 		})
+
+		const imageRef = ref(storage, `posts/${(await docRef).id}/image`)
+
+		if (image) {
+			await uploadString(imageRef, image, 'data_url').then(async () => {
+				const downloadURL = await getDownloadURL(imageRef)
+				await updateDoc(doc(db, 'posts', (await docRef).id), {
+					image: downloadURL,
+				})
+			})
+		}
+
+		setPost('')
+		setImage(null)
 	}
 
 	return (
@@ -39,11 +70,48 @@ const NewPost = () => {
 					className='newPost__form__textarea'
 					placeholder='What’s happening?'
 				/>
+				{image && (
+					<div className='newPost__form__image'>
+						<img src={image} alt='' />
+					</div>
+				)}
 				<div className='newPost__form__actions'>
-					<AiOutlinePicture />
-					<AiOutlineGif />
-					<BsEmojiSmile />
-					<HiOutlineLocationMarker />
+					<div
+						onClick={() => postPhotoRef.current.click()}
+						className='newPost__form__actions__item'
+					>
+						<AiOutlinePicture />
+						<input
+							className='newPost__form__actions__item__input'
+							type='file'
+							ref={postPhotoRef}
+							onChange={addImage}
+						/>
+					</div>
+					<div className='newPost__form__actions__item'>
+						<AiOutlineGif />
+
+						<input
+							className='newPost__form__actions__item__input'
+							type='file'
+						/>
+					</div>
+					<div className='newPost__form__actions__item'>
+						<BsEmojiSmile />
+
+						<input
+							className='newPost__form__actions__item__input'
+							type='file'
+						/>
+					</div>
+					<div className='newPost__form__actions__item'>
+						<HiOutlineLocationMarker />
+
+						<input
+							className='newPost__form__actions__item__input'
+							type='file'
+						/>
+					</div>
 
 					<button
 						onClick={savePost}
