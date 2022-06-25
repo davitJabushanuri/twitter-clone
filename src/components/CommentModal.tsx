028b184f3457ad1f370df7f808b3673ef6c7076e
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-06-26 00:26:35 +0400

    style post info in commentModal component

--- src/components/CommentModal.tsx
+++ src/components/CommentModal.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import { BiArrowBack } from 'react-icons/bi'
 import { VscChromeClose } from 'react-icons/vsc'
 import { db } from '../../firebase'
+import Moment from 'react-moment'
 
 interface Props {
 	id: string
@@ -17,7 +18,7 @@ const CommentModal = ({ id, setCommentModal }: Props) => {
 	useEffect(
 		() =>
 			onSnapshot(doc(db, 'posts', id), snapshot => {
-				setPost(snapshot?.data())
+				setPost(snapshot.data())
 			}),
 		[id, db]
 	)
@@ -30,11 +31,36 @@ const CommentModal = ({ id, setCommentModal }: Props) => {
 						onClick={() => setCommentModal(false)}
 						className='comment__header__close'
 					>
-						{window.innerWidth < 768 ? <BiArrowBack /> : <VscChromeClose />}
+						{window.innerWidth < 700 ? <BiArrowBack /> : <VscChromeClose />}
 					</div>
 				</div>
 				<button className='comment__reply'>Reply</button>
-				{post.username}
+				<div className='postInfo'>
+					<div className='postInfo__userImage'>
+						<img src={post.userImage} alt='' />
+						<div className='postInfo__userImage__line'>
+							<span></span>
+						</div>
+					</div>
+					<div className='postInfo__content'>
+						<div className='postInfo__content__userInfo'>
+							<p className='postInfo__content__userInfo__name'>{post?.user}</p>
+							<p className='postInfo__content__userInfo__username'>
+								{post?.username}
+							</p>
+							<div className='postInfo__content__userInfo__dot'></div>
+							<p className='postInfo__content__userInfo__date'>
+								<Moment fromNow ago>
+									{post.createdAt?.toDate()}
+								</Moment>
+							</p>
+						</div>
+						<div className='postInfo__content__tweet'>{post?.text}</div>
+						<div className='postInfo__content__replying'>
+							Replaying to <span>{post?.username}</span>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
